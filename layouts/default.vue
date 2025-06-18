0ada4561664a713a21bec981f293feb59431a4a5
Kris Tantinirundr <kris.ttr.dev@gmail.com>
2025-06-18 15:25:45 +0700

    feat: add user authentication and sign-up functionality
    - Updated .gitignore to exclude generated Prisma files.
    - Enhanced app configuration to include toaster default position.
    - Modified default layout to add a Sign Up link in the navigation.
    - Added sign-up page with form handling and toast notifications for success/error messages.
    - Updated package.json to include Prisma and Better Auth dependencies.
    - Created Prisma schema for TodoList and User models.
    - Implemented authentication API handler.
    - Added auth client configuration using Better Auth.
    - Set up auth utility with Prisma adapter for database integration.

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,15 @@
   <div class="p-3 text-gray-700">
     <nav>
       <div class="max-w-md mx-auto bg-gray-100 p-2 rounded-md shadow">
-        <ul>
+        <ul class="flex items-center gap-2">
           <li>
             <NuxtLink class="font-bold text-lg text-blue-600 underline" to="/"
               >Todo List</NuxtLink
+            >
+          </li>
+          <li class="ml-auto">
+            <NuxtLink class="text-white bg-blue-600 py-1 px-2 rounded-md" to="/sign-up"
+              >Sign Up</NuxtLink
             >
           </li>
         </ul>
