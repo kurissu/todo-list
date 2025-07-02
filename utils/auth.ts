fced7f66baf5d600835d648cdb2592ded82bc520
Kris Tantinirundr <kris.ttr.dev@gmail.com>
2025-07-02 20:46:45 +0700

    fix: reorder generator and datasource in Prisma schema; update import paths for PrismaClient

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,10 +1,10 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-// import { PrismaClient } from "~/generated/prisma";
+import { PrismaClient } from "~/generated/prisma";
 import { openAPI } from "better-auth/plugins"
 
 // If your Prisma file is located elsewhere, you can change the path
-import { PrismaClient } from "@prisma/client"; // Uncomment if using Prisma Client directly 
+// import { PrismaClient } from "@prisma/client"; // Uncomment if using Prisma Client directly 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
