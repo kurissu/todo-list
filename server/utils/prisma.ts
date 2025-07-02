26f508e1b84cfefb0101621bc401b5b1735e04d1
Kris Tantinirundr <kris.ttr.dev@gmail.com>
2025-07-02 21:47:00 +0700

    fix: change prisma instance export to local variable

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,4 +1,4 @@
 import { PrismaClient } from '~/generated/prisma'
 
-export const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 // use `prisma` in your application to read and write data in your DB
