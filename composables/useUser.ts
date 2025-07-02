d243232c0836401e6ed3aa84e1e661141ff37316
Kris Tantinirundr <kris.ttr.dev@gmail.com>
2025-07-02 21:43:20 +0700

    fix: throw error with message in signUp function; export prisma instance for external usage

--- composables/useUser.ts
+++ composables/useUser.ts
@@ -46,12 +46,12 @@ export function useUser() {
     const { clearTodoListOnline } = useTodo()
     clearTodoListOnline();
   }
-  async function signUp(name: string, email: string, password: string){
-    const { data, error } = await authClient.signUp.email({ name, email, password})
-    if(error){
+  async function signUp(name: string, email: string, password: string) {
+    const { data, error } = await authClient.signUp.email({ name, email, password })
+    if (error) {
       throw new Error(error.message || 'Unknown error')
     }
-    return data 
+    return data
   }
 
   return {
--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,4 +1,4 @@
 import { PrismaClient } from '~/generated/prisma'
 
-const prisma = new PrismaClient()
+export const prisma = new PrismaClient()
 // use `prisma` in your application to read and write data in your DB
