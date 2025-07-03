5d55ff34264546148d490bf92f23c67d3bffac1d
kris <kris.percussion@gmail.com>
2025-07-04 00:59:55 +0700

    remove prisma output

--- server/api/todos/create.post.ts
+++ server/api/todos/create.post.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { PrismaClient } from '~/generated/prisma'
+import { PrismaClient } from '@prisma/client';
 
 
 const schema = z.object({
--- server/api/todos/index.get.ts
+++ server/api/todos/index.get.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '~/generated/prisma'
+import { PrismaClient } from '@prisma/client';
 
 export default defineEventHandler(async (event) => {
   const prisma = new PrismaClient();
