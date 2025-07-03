3834b9fa5445a8b7bbe853c4280a9102c6cc8afb
kris <kris.percussion@gmail.com>
2025-07-04 00:51:49 +0700

    test nitro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,15 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  nitro: {
-    preset: 'vercel', // or 'vercel' for Node.js runtime
-    experimental: {
-      wasm: true
+ nitro: {
+    externals: {
+      inline: [],
     }
-  },
-  // Ensure ES modules compatibility
-  build: {
-    transpile: ['better-auth'] // Add any problematic packages here
   },
   css: ["~/assets/css/main.css"],
   ui: {
